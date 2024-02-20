d0008f605984252fc81fda9280c44d0b1c28ca46
Matt Permentier <matt.permentier@gmail.com>
2024-02-20 10:40:55 +0100

    Fix concepten

---
+++ src/Overview/Components/Concepts/ConceptListItem.tsx
@@ -0,0 +1,24 @@
+import { Card, CardContent, Typography } from '@mui/material';
+
+interface IProps {
+    title: string;
+    color: string;
+    icon: string;
+}
+
+export default function ConceptListItem(props: IProps) {
+    const { title, color, icon} = props;
+
+    return (
+        <Card sx={{ width: '100%', borderRadius: 3 }}>
+            <CardContent sx={{ bgcolor: color, height: 65 }}>
+                <Typography variant='h5' fontWeight='bold' mt={.5}>{title}</Typography>
+            </CardContent>
+
+            <CardContent sx={{ bgcolor: '#20252B', height: 250 }}>
+                <img src={icon} style={{ width: 200}}/>
+            </CardContent>
+        </Card>
+    );
+}
+
--- src/Overview/Components/Concepts/Concepts.tsx
+++ src/Overview/Components/Concepts/Concepts.tsx
@@ -1,4 +1,34 @@
+import { Box, Stack, Typography } from '@mui/material';
+import ConceptListItem from './ConceptListItem';
+import collaborativeIcon from '../../Assets/collaborative.png';
+import gamifiedIcon from '../../Assets/gamified.png';
+import interactiveIcon from '../../Assets/interactive.png';
+
 export default function Concepts() {
-    return ( <></> );
+    return ( 
+        <Box mx={30} mt={10}>
+            <Typography variant='h5' fontWeight='bold' mb={5}>Concepten</Typography>
+            
+            <Stack direction='row' justifyContent='space-between' gap={3}>
+                <ConceptListItem 
+                    title='Interactieve verhaalervaring'
+                    color='#E5F1F8'
+                    icon={interactiveIcon}
+                />
+
+                <ConceptListItem 
+                    title='Samenwerkende inhoudscreatie'
+                    color='#F4F7EA'
+                    icon={collaborativeIcon}
+                />
+
+                <ConceptListItem 
+                    title='Gamified kijkervaring'
+                    color='#F1E8F8'
+                    icon={gamifiedIcon}
+                />
+            </Stack>
+        </Box> 
+    );
 }
 
